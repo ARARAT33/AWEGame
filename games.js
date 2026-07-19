915448b94a98a4644f3fbba0647eb4e1c2750317
Ararat <araratavetisyan777999@gmail.com>
2026-07-19 18:16:59 +0400

    Update games.js

--- games.js
+++ games.js
@@ -2,11 +2,11 @@
 
 const games = [
   {
-    title: "",
-    folder: "",
-    description: "",
-    developer: "",
-    icon: ""
+    title: "Random Links",
+    folder: "randomlink.html",
+    description: "It's a game that discovers links across the web that you would otherwise never find. Always see something brand new!",
+    developer: "AWEGames",
+    icon: "✈"
   },
   
 ];
